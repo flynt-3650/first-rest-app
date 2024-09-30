b161966e2978cfd0e6f4a0cb29a922f361d3062b
flynt3650 <repilovi@gmail.com>
2024-09-30 13:43:24 +0300

    initial setup

---
+++ src/test/java/ru/flynt3650/project/first_rest_app/FirstRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flynt3650.project.first_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
