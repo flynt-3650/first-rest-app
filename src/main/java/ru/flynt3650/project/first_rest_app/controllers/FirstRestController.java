bfa206035c6c8eb8855322c005372fb0cd4022b8
flynt3650 <repilovi@gmail.com>
2024-09-30 13:52:34 +0300

    implement rest controller

---
+++ src/main/java/ru/flynt3650/project/first_rest_app/controllers/FirstRestController.java
@@ -0,0 +1,15 @@
+package ru.flynt3650.project.first_rest_app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FirstRestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
